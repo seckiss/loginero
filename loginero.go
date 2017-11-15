76b450b275e69f59f1df6048d6ac3ed27a369fec
greg-sk <grzegorz.luczywo@securitykiss.com>
2017-11-15 12:12:55 +0100

    work in progress

--- loginero.go
+++ loginero.go
@@ -28,31 +28,33 @@ func SetOptions() {
 	//TODO set BID and SID cookie template (Path, Secure, HttpOnly, MaxAge, etc)
 }
 
+type SimpleUser struct {
+	Username string
+}
+
 type RamUserStore struct {
 }
 
-func (store *RamUserStore) CreateUserCreds(r *http.Request) interface{} {
+func (store *RamUserStore) CreateUserCreds(r *http.Request, bid string) interface{} {
 	return nil
 }
-func (store *RamUserStore) FindUserCreds(r *http.Request) interface{} {
+func (store *RamUserStore) FindUserCreds(r *http.Request, bid string) interface{} {
 	return nil
 }
-func (store *RamUserStore) GetSIDUser(sid string) interface{} {
+func (store *RamUserStore) GetSessionUser(sid string) interface{} {
 	return nil
 }
-func (store *RamUserStore) GetBIDUser(bid string) interface{} {
+func (store *RamUserStore) GetBrowserUser(bid string) interface{} {
 	return nil
 }
-func (store *RamUserStore) SaveSIDUser(sid string, user interface{}) {
+func (store *RamUserStore) SaveSessionUser(sid string, user interface{}) {
 
 }
-func (store *RamUserStore) SaveBIDUser(bid string, user interface{}) {
+func (store *RamUserStore) CreateBrowserUser(bid string) interface{} {
+	return nil
 
 }
-func (store *RamUserStore) DeleteSIDUser(sid string) {
-
-}
-func (store *RamUserStore) DeleteBIDUser(bid string) {
+func (store *RamUserStore) DeleteSessionUser(sid string) {
 
 }
 
@@ -60,16 +62,19 @@ type UserStore interface {
 	// use credentials from the request to create the new user object
 	// store it in db and return it (without credentials)
 	// return nil if user already exists (unique by username/email/id etc)
-	CreateUserCreds(r *http.Request) interface{}
+	// the bid argument may be used to link the anonymous BrowserUser (BIDuser)
+	// with the newly created account/user
+	CreateUserCreds(r *http.Request, bid string) interface{}
 	// use credentials from the request to find user in the db
 	// return it (without credentials) or nil if not found
-	FindUserCreds(r *http.Request) interface{}
-	GetSIDUser(sid string) interface{}
-	GetBIDUser(bid string) interface{}
-	SaveSIDUser(sid string, user interface{})
-	SaveBIDUser(bid string, user interface{})
-	DeleteSIDUser(sid string)
-	DeleteBIDUser(bid string)
+	// the bid argument may be used to link the anonymous BrowserUser (BIDuser)
+	// with the credential based logging in user
+	FindUserCreds(r *http.Request, bid string) interface{}
+	GetSessionUser(sid string) interface{}
+	GetBrowserUser(bid string) interface{}
+	SaveSessionUser(sid string, user interface{})
+	CreateBrowserUser(bid string) interface{}
+	DeleteSessionUser(sid string)
 }
 
 func LoginHandler(redirectSuccess string, redirectFail string) http.HandlerFunc {
@@ -79,20 +84,91 @@ func LoginHandler(redirectSuccess string, redirectFail string) http.HandlerFunc 
 			bid = generateID()
 		}
 		setBIDCookie(w, bid)
-		user := defaultUserStore.FindUserCreds(r)
+		user := defaultUserStore.FindUserCreds(r, bid)
 		if user != nil {
 			sid := generateID()
 			setSIDCookie(w, sid)
-			defaultUserStore.SaveSIDUser(sid, user)
+			defaultUserStore.SaveSessionUser(sid, user)
 			http.Redirect(w, r, redirectSuccess, http.StatusSeeOther)
 		} else {
 			sid := getRequestSID(r)
 			if sid != "" {
 				deleteSIDCookie(w)
-				defaultUserStore.DeleteSIDUser(sid)
+				defaultUserStore.DeleteSessionUser(sid)
 			}
 			http.Redirect(w, r, redirectFail, http.StatusSeeOther)
 		}
+	}
+}
+
+func CreateAccountHandler(redirectSuccess string, redirectFail string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		bid := getRequestBID(r)
+		if bid == "" {
+			bid = generateID()
+		}
+		setBIDCookie(w, bid)
+		user := defaultUserStore.CreateUserCreds(r, bid)
+		if user != nil {
+			sid := generateID()
+			setSIDCookie(w, sid)
+			defaultUserStore.SaveSessionUser(sid, user)
+			http.Redirect(w, r, redirectSuccess, http.StatusSeeOther)
+		} else {
+			sid := getRequestSID(r)
+			if sid != "" {
+				deleteSIDCookie(w)
+				defaultUserStore.DeleteSessionUser(sid)
+			}
+			http.Redirect(w, r, redirectFail, http.StatusSeeOther)
+		}
+	}
+}
+
+func LogoutHandler(redirectSuccess string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		bid := getRequestBID(r)
+		if bid == "" {
+			bid = generateID()
+		}
+		setBIDCookie(w, bid)
+		sid := getRequestSID(r)
+		if sid != "" {
+			deleteSIDCookie(w)
+			defaultUserStore.DeleteSessionUser(sid)
+		}
+		http.Redirect(w, r, redirectSuccess, http.StatusSeeOther)
+	}
+}
+
+func PageHandler(loggedHandler http.HandlerFunc, unloggedHandler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user interface{}
+		sid := getRequestSID(r)
+		if sid != "" {
+			user = defaultUserStore.GetSessionUser(sid)
+			if user != nil {
+				setSIDCookie(w, sid)
+				//TODO save user data in context[r]
+				loggedHandler(w, r)
+				//TODO delete user data from context[r]
+				return
+			} else {
+				deleteSIDCookie(w)
+			}
+		}
+
+		bid := getRequestBID(r)
+		if bid == "" {
+			bid = generateID()
+			user = defaultUserStore.CreateBrowserUser(bid)
+		} else {
+			user = defaultUserStore.GetBrowserUser(bid)
+		}
+		setBIDCookie(w, bid)
+		//TODO save user data in context[r]
+		unloggedHandler(w, r)
+		//TODO delete user data from context[r]
 	}
 }
 

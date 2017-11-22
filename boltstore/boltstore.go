5fe66514c8643e2904cbbe468a2d48a54a0db3e3
greg-sk <grzegorz.luczywo@securitykiss.com>
2017-11-22 13:42:43 +0100

    work in progress

--- boltstore/boltstore.go
+++ boltstore/boltstore.go
@@ -3,26 +3,95 @@ package boltstore
 import (
 	"bytes"
 	"encoding/gob"
+	"errors"
+	"time"
+
+	"github.com/boltdb/bolt"
 )
 
-var network bytes.Buffer // Stand-in for the network.
-var enc = gob.NewEncoder(&network)
-var dec = gob.NewDecoder(&network)
-
-func Put(p interface{}) error {
-	// The encode will fail unless the concrete type has been
-	// registered. We registered it in the calling function.
-
-	// Pass pointer to interface so Encode sees (and hence sends) a value of
-	// interface type. If we passed p directly it would see the concrete type instead.
-	// See the blog post, "The Laws of Reflection" for background.
-	return enc.Encode(&p)
+// KVStore represents the key value store. Use the Open() method to create
+// one, and Close() it when done.
+type BoltStore struct {
+	db *bolt.DB
 }
 
-func Get() (interface{}, error) {
-	// The decode will fail unless the concrete type on the wire has been
-	// registered. We registered it in the calling function.
+var (
+	// ErrNotFound is returned when the key supplied to a Get or Delete
+	// method does not exist in the database.
+	ErrNotFound = errors.New("skv: key not found")
+
+	// ErrBadValue is returned when the value supplied to the Put method
+	// is nil.
+	ErrBadValue = errors.New("skv: bad value")
+
+	bucketName = []byte("kv")
+)
+
+// Open a key-value store. "path" is the full path to the database file, any
+// leading directories must have been created already. File is created with
+// mode 0640 if needed.
+//
+// Because of BoltDB restrictions, only one process may open the file at a
+// time. Attempts to open the file from another process will fail with a
+// timeout error.
+func Open(path string) (*BoltStore, error) {
+	opts := &bolt.Options{
+		Timeout: 50 * time.Millisecond,
+	}
+	if db, err := bolt.Open(path, 0640, opts); err != nil {
+		return nil, err
+	} else {
+		err := db.Update(func(tx *bolt.Tx) error {
+			_, err := tx.CreateBucketIfNotExists(bucketName)
+			return err
+		})
+		if err != nil {
+			return nil, err
+		} else {
+			return &BoltStore{db: db}, nil
+		}
+	}
+}
+
+func (kvs *BoltStore) Put(key string, value interface{}) error {
+	if value == nil {
+		return ErrBadValue
+	}
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(&value); err != nil {
+		return nil
+	}
+	return kvs.db.Update(func(tx *bolt.Tx) error {
+		return tx.Bucket(bucketName).Put([]byte(key), buf.Bytes())
+	})
+}
+
+func (kvs *BoltStore) Get(key string) (interface{}, error) {
 	var p interface{}
-	err := dec.Decode(&p)
+	err := kvs.db.View(func(tx *bolt.Tx) error {
+		c := tx.Bucket(bucketName).Cursor()
+		if k, v := c.Seek([]byte(key)); k == nil || string(k) != key {
+			return ErrNotFound
+		} else {
+			d := gob.NewDecoder(bytes.NewReader(v))
+			return d.Decode(&p)
+		}
+	})
 	return p, err
+}
+
+func (kvs *BoltStore) Delete(key string) error {
+	return kvs.db.Update(func(tx *bolt.Tx) error {
+		c := tx.Bucket(bucketName).Cursor()
+		if k, _ := c.Seek([]byte(key)); k == nil || string(k) != key {
+			return ErrNotFound
+		} else {
+			return c.Delete()
+		}
+	})
+}
+
+// Close closes the key-value store file.
+func (kvs *BoltStore) Close() error {
+	return kvs.db.Close()
 }
--- examples/kvtest/kvtest.go
+++ examples/kvtest/kvtest.go
@@ -12,16 +12,23 @@ type Point struct {
 
 func main() {
 	gob.Register(Point{})
+	_ = gob.Register
 
-	//boltstore.Put("eeee")
-	err := boltstore.Put(Point{X: 3, Y: 7})
+	bs, err := boltstore.Open("/tmp/kvtest.db")
 	if err != nil {
 		panic(err)
 	}
-	res, err := boltstore.Get()
+
+	err = bs.Put("key1", Point{X: 3, Y: 7})
+	if err != nil {
+		panic(err)
+	}
+	res, err := bs.Get("key1")
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("res=%+v\n", res)
+
+	bs.Close()
 }
